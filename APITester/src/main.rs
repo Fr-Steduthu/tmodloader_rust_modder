ecc50b25c796ca4fcaebb6a540fa6566f5d61897
Erwann Saint Germain <75694994+Fr-Steduthu@users.noreply.github.com>
2022-12-17 21:18:14 +0100

    Added tests

--- APITester/src/main.rs
+++ APITester/src/main.rs
@@ -3,5 +3,6 @@ fn main() {
     println!("Hello, world!");
 
     let item = tmodloader_api::tmod_types::Item::new("tyuhvb".to_string(), "tyughuj".to_string());
-    println!("{}", serde_json::to_string(&item).unwrap())
+    println!("{}", serde_json::to_string(&item.clone()).unwrap());
+    println!("{}", item.to_string());
 }
