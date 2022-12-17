e88a57b7c170e48ac2e739a2837bb27957f70473
Erwann Saint Germain <75694994+Fr-Steduthu@users.noreply.github.com>
2022-12-17 16:08:05 +0100

    Item::new added

--- src/tmodloader_api/src/tmod_types/mod.rs
+++ src/tmodloader_api/src/tmod_types/mod.rs
@@ -227,10 +227,47 @@ pub struct Item {
     damage_type : DamageType,
     knockback : u16,
 
-    shoot : ProjectileId,
-    shoot_speed : self::Time,
+    shoot : Option<ProjectileId>,
+    shoot_speed : Option<self::Time>,
+    useAmmo : Option<ItemId>,
 
     heal_life: u16,
+}
+
+impl Item {
+    fn new(name : String, tooltip : String) -> Self {
+        Item{
+            id: Identifier { id: name, is_vanilla: false },
+            name: name,
+            tooltip: tooltip,
+            value: Value {
+                platinum: 0,
+                gold: 0,
+                silver: 0,
+                copper: 0,
+            },
+            rarity: 0.into(),
+            max_stack: 1,
+            width: 0,
+            height: 0,
+            use_time: 0,
+            use_animation: 0,
+            use_style: UseStyle::Swing,
+            use_sound: crate::terraria_defaults::sound_ids::items::Melee,
+            auto_reuse: false,
+            consumable: false,
+            no_use_graphics: false,
+            use_turn: false,
+            no_melee: false,
+            damage: 0,
+            damage_type: DamageType::Melee,
+            knockback: 0,
+            shoot: Identifier { id: "".to_string(), is_vanilla: false },
+            shoot_speed: 0,
+            useAmmo: Identifier { id: "".to_string(), is_vanilla: false },
+            heal_life: 0,
+        }
+    }
 }
 
 impl Into<CSType> for Item {
