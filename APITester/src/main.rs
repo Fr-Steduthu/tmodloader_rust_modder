720ea60b702426d48033020f4c294c35be7a467b
Erwann Saint Germain <75694994+Fr-Steduthu@users.noreply.github.com>
2022-12-18 00:22:00 +0100

    Update main.rs

--- APITester/src/main.rs
+++ APITester/src/main.rs
@@ -1,8 +1,9 @@
+use tmodloader_api::cs_types::CSObject;
 
 fn main() {
     println!("Hello, world!");
 
     let item = tmodloader_api::tmod_types::Item::new("tyuhvb".to_string(), "tyughuj".to_string());
     println!("{}", serde_json::to_string(&item.clone()).unwrap());
-    println!("{}", item.to_string());
+    println!("{}", <tmodloader_api::tmod_types::Item as Into<CSObject>>::into(item).to_string());
 }
