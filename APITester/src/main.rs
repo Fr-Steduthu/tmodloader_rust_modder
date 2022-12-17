87194e61ba839b07337fd72d160496ea6ba3dcef
Erwann Saint Germain <75694994+Fr-Steduthu@users.noreply.github.com>
2022-12-17 18:17:22 +0100

    Update API tester

--- APITester/src/main.rs
+++ APITester/src/main.rs
@@ -1,3 +1,7 @@
+
 fn main() {
     println!("Hello, world!");
+
+    let item = tmodloader_api::tmod_types::Item::new("tyuhvb".to_string(), "tyughuj".to_string());
+    println!("{}", serde_json::to_string(&item).unwrap())
 }
