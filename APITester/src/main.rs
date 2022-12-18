12ebf2c67f13ae98c97b9a25efa3e287c94fe0f6
Erwann Saint Germain <75694994+Fr-Steduthu@users.noreply.github.com>
2022-12-18 11:48:30 +0100

    Tests

--- APITester/src/main.rs
+++ APITester/src/main.rs
@@ -1,9 +1,31 @@
-use tmodloader_api::cs_types::CSObject;
+#![allow(non_snake_case)]
+
+use tmodloader_api::{
+    cs_types::CSObject,
+    tmod_types::{
+        Item,
+    }
+};
+
+#[cfg(test)]
+macro_rules! startup {
+    ( $item : ident ) => {
+        let $item : Item = Item::new("Dummy item".to_string(), "Item created for testing purposes".to_string() );
+    };
+}
 
 fn main() {
     println!("Hello, world!");
 
-    let item = tmodloader_api::tmod_types::Item::new("tyuhvb".to_string(), "tyughuj".to_string());
+    let item = Item::new("tyuhvb".to_string(), "tyughuj".to_string());
     println!("{}", serde_json::to_string(&item.clone()).unwrap());
-    println!("{}", <tmodloader_api::tmod_types::Item as Into<CSObject>>::into(item).to_string());
+    println!("{}", <Item as Into<CSObject>>::into(item).to_string());
+}
+
+
+
+#[test]
+fn test_item_to_csobj(){
+    startup!(item);
+    println!("{:?}", <Item as Into<CSObject>>::into(item));
 }
